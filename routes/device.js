a5f245e921fdb14f16a31ef6e59d283f9eb20615
cndtjq010 <cndtjq010@naver.com>
2020-12-06 16:36:35 +0900

    fix : update device.js

--- routes/device.js
+++ routes/device.js
@@ -92,22 +92,16 @@ router.put('/info/:device_id',(req,res)=>
     });
 });
 
-router.get('/run/:device_id',(req,res)=>
+router.get('/run/:device_id/:device_type',(req,res)=>
 {   
-    Device.findOne({device_id:req.params.device_id},function(err,data)
-    {
-        mqttClient.publish(req.params.device_id,'khulock run '+data.device_type, qos=2);
-            res.send("Send message to "+req.params.device_id);
-    }
+        mqttClient.publish(req.params.device_id,'khulockrun '+req.params.device_type, qos=2);
+        res.send("Send message to "+req.params.device_id);
 });
 
-router.get('/stop/:device_id',(req,res)=>
+router.get('/stop/:device_id/:device_type',(req,res)=>
 {
-    Device.findOne({device_id:req.params.device_id},function(err,data)
-    {
-        mqttClient.publish(req.params.device_id,'khulock stop '+data.device_type, qos=2);
-        res.send("Send message to "+req.params.device_id);
-    }
+    mqttClient.publish(req.params.device_id,'khulockstop '+req.params.device_type, qos=2);
+    res.send("Send message to "+req.params.device_id);
 });
 
 router.get('/:device_id',(req,res)=>
